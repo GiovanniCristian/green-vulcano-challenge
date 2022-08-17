b668ced858adc4232b9223ff72f9ab682b3d43b9
Giovanni Cristian Marrocco <marroccogc@gmail.com>
2022-08-17 17:59:49 +0200

    inizializzato progetto e creata tabella esempio

---
+++ src/Table.js
@@ -0,0 +1,113 @@
+import React from "react";
+import { usePagination, useTable } from "react-table";
+import { useGlobalFilter, useAsyncDebounce} from "react-table";
+
+function GlobalFilter({
+    preGlobalFilteredRows,
+    globalFilter,
+    setGlobalFilter,
+  }) {
+    const count = preGlobalFilteredRows.length
+    const [value, setValue] = React.useState(globalFilter)
+    const onChange = useAsyncDebounce(value => {
+      setGlobalFilter(value || undefined)
+    }, 200)
+  
+    return (
+      <span>
+        Search:{' '}
+        <input
+          value={value || ""}
+          onChange={e => {
+            setValue(e.target.value);
+            onChange(e.target.value);
+          }}
+          placeholder={`${count} records...`}
+        />
+      </span>
+    )
+}
+
+function Table({ columns, data }) {
+  const { 
+    getTableProps, 
+    getTableBodyProps, 
+    headerGroups, 
+    page, 
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    state, 
+    preGlobalFilteredRows, 
+    setGlobalFilter
+    } =
+    useTable({
+      columns,
+      data,
+    },
+    useGlobalFilter,
+    usePagination,
+    )
+
+  return (
+<>
+    <GlobalFilter
+        preGlobalFilteredRows={preGlobalFilteredRows}
+        globalFilter={state.globalFilter}
+        setGlobalFilter={setGlobalFilter}
+      />
+
+    <table {...getTableProps()} border="1">
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {page.map((page, i) => {
+          prepareRow(page);
+          return (
+            <tr {...page.getRowProps()}>
+              {page.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+    <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {state.pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+      </div>
+</>
+
+  );
+}
+
+export default Table;
